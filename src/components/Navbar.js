96145021d18009bfaf4747ef702eae6076f21775
Ross Cumming <rosso5792@gmail.com>
2020-12-10 11:04:13 +0000

    reload element created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../css/imgs/spacex-logo.png";
+import reload from "../css/icons/refresh.png";
 
 const Navbar = () => {
   return (
@@ -10,6 +11,10 @@ const Navbar = () => {
         alt="SpaceX's official company Logo"
       ></img>
       <h1 className="nav__title">Launches</h1>
+      <button className="nav__reload-button">
+        <p className="nav__reload-text">Reload Data</p>
+        <img src={reload} alt="launch information reload button" />
+      </button>
     </nav>
   );
 };

35ffed7aa44a097e6becb5d0203765839f732dbb
Ross Cumming <rosso5792@gmail.com>
2020-12-10 15:31:14 +0000

    filters buttons created, aligned, moving onto launch items

--- src/components/LaunchFilters.js
+++ src/components/LaunchFilters.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Sort from "../css/icons/sort.png";
 
 const LaunchFilters = () => {
-  return <div></div>;
+  return (
+    <div className="main__launchfilters">
+      <select className="main__launchfilters-year--select">
+        <option value="default-value">Filter By Year</option>
+      </select>
+      <button className="main__launchfilters-year--sort">
+        <p className="main__launchfilters-sort-button">Sort Descending</p>
+        <img src={Sort} alt="sort launch list by ascending or descending"></img>
+      </button>
+    </div>
+  );
 };
 
 export default LaunchFilters;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,8 @@ import React from "react";
 import LaunchHome from "../css/imgs/launch-home.png";
 import LaunchHome2x from "../css/imgs/launch-home@2x.png";
 import LaunchHome3x from "../css/imgs/launch-home@3x.png";
+import LaunchFilters from "./LaunchFilters";
+import LaunchList from "./LaunchList";
 
 const Main = () => {
   return (
@@ -10,6 +12,11 @@ const Main = () => {
         <source srcSet={`${LaunchHome}, ${LaunchHome2x}, ${LaunchHome3x}`} />
         <img src={LaunchHome} alt="rocket taking off to space" />
       </picture>
+
+      <div className="main__launchlist-container">
+        <LaunchFilters />
+        <LaunchList />
+      </div>
     </main>
   );
 };
